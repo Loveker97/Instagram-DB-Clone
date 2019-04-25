840b84c76c03696de72136a36d411282905e3d24
Loveker97 <44221941+Loveker97@users.noreply.github.com>
2019-04-25 10:13:52 -0700

    Add files via upload

--- IG_clone.sql
+++ IG_clone.sql
@@ -20,7 +20,7 @@ CREATE TABLE comments(
     comment_text VARCHAR(255) NOT NULL, 
     user_id INT NOT NULL,
     photo_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW,
+    created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
@@ -29,7 +29,7 @@ CREATE TABLE likes(
     user_id INT NOT NULL,
     photo_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
 );
